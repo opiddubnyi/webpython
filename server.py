11b085df3a6a42c6c02c65402f4cf3c7c0ff5516
Oleksandr Piddubnyi <kakadrilus@gmail.com>
2021-05-10 15:51:25 -0700

    init

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_socket.connect(('data.pr4e.org', 80))
+
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+
+my_socket.send(cmd)
+
+while True:
+    data = my_socket.recv(512)
+    if not data:
+        break
+    print(data.decode())
+my_socket.close()
