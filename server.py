101c6d2df214267dace60c2d000bb5b1e392307d
Oleksandr Piddubnyi <kakadrilus@gmail.com>
2021-05-10 17:14:50 -0700

    testing some stuff

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
 import socket
+import requests
+import re
+# my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# my_socket.connect(('data.pr4e.org', 80))
+#
+# cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+#
+# my_socket.send(cmd)
+#
+# while True:
+#     data = my_socket.recv(512)
+#     if not data:
+#         break
+#     print(data.decode())
+# my_socket.close()
 
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.connect(('data.pr4e.org', 80))
+url = 'http://data.pr4e.org/intro-short.txt'
+resp = requests.get(url)
 
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
-
-my_socket.send(cmd)
-
-while True:
-    data = my_socket.recv(512)
-    if not data:
-        break
-    print(data.decode())
-my_socket.close()
-print('test')
+print(resp.headers['Last-Modified'])
+print(resp.headers['ETag'])
+print(resp.headers['Content-Length'])
+print(resp.headers['Cache-Control'])
+print(resp.headers['Content-Type'])
