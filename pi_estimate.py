e8883ab42e80cd59d9302746e6f27e2f3407a4ad
Oleksandr Piddubnyi <kakadrilus@gmail.com>
2021-12-08 11:43:04 -0800

    test

--- pi_estimate.py
+++ pi_estimate.py
@@ -4,15 +4,15 @@ tries = 10000
 in_count = 0
 out_count = 0
 for i in range(1, tries):
-    x = random.uniform(0,1)
-    y = random.uniform(0,1)
+    x = random.uniform(0, 1)
+    y = random.uniform(0, 1)
+    g = random.uniform(0, 1)
 
-    c = x**2 + y**2
+    c = x ** 2 + y ** 2
 
     if c <= 1:
         in_count += 1
     else:
         out_count += 1
 
-print('pi is around ~', 4 * in_count/(out_count + in_count))
-
+print('pi is around ~', 4 * in_count / (out_count + in_count))
